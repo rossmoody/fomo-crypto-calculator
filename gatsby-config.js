9d25710589cedd0ea9e1b7e23fb3b1902dfd1ba2
rossmoody <fiftyfivehis@gmail.com>
2021-03-17 10:15:09 -0700

    update names

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    title: "FOMO Crypto Calculator"
   },
   plugins: [
     "gatsby-plugin-netlify-cms",
@@ -8,16 +8,16 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: "G-DLZ5XDZ406",
-      },
+        trackingId: "G-DLZ5XDZ406"
+      }
     },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
-      },
+        icon: "src/images/icon.png"
+      }
     },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
@@ -25,9 +25,9 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: "./src/images/"
       },
-      __key: "images",
-    },
-  ],
-};
+      __key: "images"
+    }
+  ]
+}
