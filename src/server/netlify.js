ccc55fc2c5466eece9ed45eb5e1200dd2ab5102e
rossmoody <fiftyfivehis@gmail.com>
2021-03-17 18:47:15 -0700

    hello world serverless

--- src/server/netlify.js
+++ src/server/netlify.js
@@ -1,1 +1,6 @@
-console.log("this is server code")
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello World" })
+  }
+}
