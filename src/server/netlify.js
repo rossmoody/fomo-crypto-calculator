818417791f7699c60a8ced3f14ec282ce7bccde3
rossmoody <fiftyfivehis@gmail.com>
2021-03-17 19:05:01 -0700

    Update netlify.js

--- src/server/netlify.js
+++ src/server/netlify.js
@@ -1,6 +1,6 @@
-exports.handler = async function (event, context) {
+export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" })
+    body: JSON.stringify({ message: "Hi there" })
   }
 }
