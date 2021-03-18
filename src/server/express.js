3d03e3365ac71622d85237b542770ca016539a76
rossmoody <fiftyfivehis@gmail.com>
2021-03-17 20:14:15 -0700

    fetch stuff

---
+++ src/server/express.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch")
+const express = require("express")
+const app = express()
+const port = 8000
+
+app.get("/.netlify/functions/netlify", async (req, res) => {
+  const response = await fetch(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.COIN_MARKET_KEY
+      }
+    }
+  )
+  const body = await response.text()
+  res.send(body)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
--- src/server/netlify.js
+++ src/server/netlify.js
@@ -1,8 +1,18 @@
+const fetch = require("node-fetch")
+
 exports.handler = async function (event, context) {
-  console.log("Where does this end up?")
-  console.log(event, context)
+  const response = await fetch(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    {
+      headers: {
+        "X-CMC_PRO_API_KEY": process.env.COIN_MARKET_KEY
+      }
+    }
+  )
+  const body = await response.text()
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" })
+    body: JSON.stringify(body)
   }
 }
