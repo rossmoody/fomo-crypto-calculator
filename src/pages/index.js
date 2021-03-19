4f7fba9c3e9b38509a87633e78d59034ba0cf730
rossmoody <fiftyfivehis@gmail.com>
2021-03-19 15:52:16 -0700

    update scripts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,8 @@
 import * as React from "react"
-
-async function test() {
-  const todaysCryptoData =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  const historicalCryptoData =
-    "https://api.coingecko.com/api/v3/coins/bitcoin/history?date=30-12-2017"
-  const response = await fetch(historicalCryptoData)
-  const body = await response.json()
-  console.log(body.market_data.current_price.usd)
-}
-
-test()
+import test from "../components/ticker"
 
 const IndexPage = () => {
+  test()
   return (
     <>
       <header>
