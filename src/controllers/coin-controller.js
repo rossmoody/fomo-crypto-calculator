405a486ece2ebdb472f475c7cd68af91dfa1af39
rossmoody <fiftyfivehis@gmail.com>
2021-03-25 19:09:24 -0700

    Update coin-controller.js

--- src/controllers/coin-controller.js
+++ src/controllers/coin-controller.js
@@ -52,7 +52,7 @@ class Coin {
 
 const getCryptoData = async () => {
   const response = await axios.get("/.netlify/functions/netlify")
-  const data = response.body
+  const data = response.data
   console.log(data)
 
   const todaysTop100 = await coingecko.get(
