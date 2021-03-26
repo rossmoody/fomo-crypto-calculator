f6013fdf9ce68df5750468498c723807bf1ba010
rossmoody <fiftyfivehis@gmail.com>
2021-03-25 22:12:27 -0700

    Update history.js

--- server/history.js
+++ server/history.js
@@ -8,15 +8,15 @@ const coingecko = axios.create({
 })
 
 exports.handler = async function (event, context) {
-  console.log("event", event)
   const response = await coingecko.get(
     `/${event.queryStringParamaters.id}/history?date=${event.queryStringParamaters.history}`
   )
-  console.log("response", response)
-  // const price = response.data.market_data?.current_price.usd
+  const price = response.data.market_data?.current_price.usd
 
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify(price)
-  // }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      price: price
+    })
+  }
 }
