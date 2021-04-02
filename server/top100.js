ce5f54e1e783cfb715e1b86e7576c1ce2b4df35f
rossmoody <fiftyfivehis@gmail.com>
2021-04-01 23:35:02 -0700

    Update top100.js

--- server/top100.js
+++ server/top100.js
@@ -1,20 +1,20 @@
 const axios = require("axios")
 const axiosRetry = require("axios-retry")
 
-const coingecko = axios.create({
-  baseURL: "https://api.coingecko.com/api/v3/coins",
-  headers: {
-    "Content-Type": "application/json"
-  }
-})
-
-axiosRetry(coingecko, {
-  retryDelay: retryCount => {
-    return retryCount * 2000
-  }
-})
-
 exports.handler = async function (event, context) {
+  const coingecko = axios.create({
+    baseURL: "https://api.coingecko.com/api/v3/coins",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+
+  axiosRetry(coingecko, {
+    retryDelay: retryCount => {
+      return retryCount * 2000
+    }
+  })
+
   const url =
     "/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
 
