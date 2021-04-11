ba4ec385c1b0dbce44ce5cfb88234a1113b16b13
rossmoody <fiftyfivehis@gmail.com>
2021-04-10 17:07:20 -0700

    better fetch version

--- server/price-history.js
+++ server/price-history.js
@@ -4,23 +4,23 @@ exports.handler = async function (event) {
   const id = event.queryStringParameters.id
   const history = event.queryStringParameters.history
 
+  let price = 0
+
   try {
     const coinRef = database.ref("coins").child(id)
     const snapshot = (await coinRef.once("value")).val()
 
     for (const obj of Object.values(snapshot)) {
       if (obj.date === history) {
-        return {
-          statusCode: 200,
-          body: JSON.stringify(obj.price)
-        }
+        price = obj.price
       }
     }
   } catch (error) {
-    console.log(`Error getting past price of ${id} on ${history}`)
-    return {
-      statusCode: 200,
-      body: JSON.stringify(0)
-    }
+    console.log(`Error getting past price of ${id} on ${history}`, error)
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(price)
   }
 }
