53b4bfb383e8031a1b06d38b6faef046eb66502a
rossmoody <fiftyfivehis@gmail.com>
2021-04-12 18:56:54 -0700

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,14 @@
-import Coin from "./coin/coin"
-import CoinItem from "./coin-item/coin-item"
-import CoinList from "./coin-list/coin-list"
-import FadeIn from "./fade-in/fade-in"
-import Footer from "./footer/footer"
-import Graveyard from "./graveyard/graveyard"
-import Header from "./header/header"
-import Hero from "./hero/hero"
-import HomePage from "./home/home"
-import Loader from "./loader/loader"
-import Logo from "./logo/logo"
-import ProfitLoss from "./profit-loss/profit-loss"
+import Coin from './coin/coin'
+import CoinItem from './coin-item/coin-item'
+import CoinList from './coin-list/coin-list'
+import FadeIn from './fade-in/fade-in'
+import Footer from './footer/footer'
+import Graveyard from './graveyard/graveyard'
+import Header from './header/header'
+import Hero from './hero/hero'
+import Loader from './loader/loader'
+import Logo from './logo/logo'
+import ProfitLoss from './profit-loss/profit-loss'
 
 export {
   Coin,
@@ -20,7 +19,6 @@ export {
   Graveyard,
   Header,
   Hero,
-  HomePage,
   Loader,
   Logo,
   ProfitLoss
