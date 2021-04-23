519de72f56a5e4ef91bba8a391dae34a21e2327e
rossmoody <fiftyfivehis@gmail.com>
2021-04-23 16:12:58 -0700

    checkpoint

--- src/components/graveyard/index.tsx
+++ src/components/graveyard/index.tsx
@@ -1,21 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Box, Flex, VStack, Text, SlideFade, Divider } from '@chakra-ui/react'
 import { getContext, Tombstone } from '../'
+
+const debounce = (callback, wait) => {
+  let timeoutId = null
+
+  return (...args) => {
+    window.clearTimeout(timeoutId)
+    timeoutId = window.setTimeout(() => {
+      callback(args)
+    }, wait)
+  }
+}
 
 export const Graveyard = (): JSX.Element => {
   const { state } = getContext()
 
   const [open, setOpen] = useState(false)
-
-  let timer
+  const [tombstones, setTombstones] = useState([])
 
   useEffect(() => {
     setOpen(false)
-    clearTimeout(timer)
-    timer = setTimeout(() => {
-      if (state.coins.filter((coin) => !coin.past_price).length != 0)
-        setOpen(true)
-    }, 2000)
+
+    const debounceTombstones = debounce(() => {
+      setTombstones(
+        state.coins
+          .filter((coin) => !coin.past_price)
+          .sort((a, b) => {
+            if (a.id < b.id) return -1
+          })
+      )
+
+      setTimeout(setOpen, 1000, true)
+    }, 1000)
+
+    debounceTombstones()
   }, [state.coins])
 
   return (
@@ -27,7 +46,7 @@ export const Graveyard = (): JSX.Element => {
       display={open ? 'flex' : 'none'}
     >
       <Box width='2xl'>
-        <SlideFade in={open} offsetY='20px' reverse>
+        <SlideFade in={open} offsetY='20px'>
           <VStack px={[2, 4]} spacing={4}>
             <Divider mb={8} />
             <Text mb={3} textAlign='center' color='gray.500'>
@@ -40,14 +59,9 @@ export const Graveyard = (): JSX.Element => {
               flexWrap='wrap'
               maxW='xl'
             >
-              {state.coins
-                .filter((coin) => !coin.past_price)
-                .sort((a, b) => {
-                  if (a.id < b.id) return -1
-                })
-                .map((coin, index) => {
-                  return <Tombstone coin={coin} key={index} />
-                })}
+              {tombstones.map((coin, index) => {
+                return <Tombstone coin={coin} key={index} />
+              })}
             </Flex>
           </VStack>
         </SlideFade>
