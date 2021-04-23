9aff11d8dbe6a6435bd7122bf8cc0f80b7f4b172
rossmoody <fiftyfivehis@gmail.com>
2021-04-23 12:24:12 -0700

    viewpoert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,6 +33,10 @@ const IndexPage = (): JSX.Element => {
         <meta name='twitter:title' content={meta.title} />
         <meta property='twitter:description' content={meta.description} />
         <meta property='twitter:image' content={`${meta.url}/og-image.png`} />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1.0'
+        ></meta>
       </Helmet>
       <Header />
       <Hero />
